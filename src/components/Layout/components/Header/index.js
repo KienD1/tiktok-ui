0392d2f2b601542afd87e1e4df92a6e14a273a99
Tran Trung Kien <kientt1@vmodev.com>
2023-07-06 08:56:42 +0700

    feat(frontend): ui login button 2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard, faCloudUpload, faMessage } from '@fortawesome/free-solid-svg-icons'
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css'; // optional
 import styles from './Header.module.scss'
-import images from '~/assets/image';
+import logoDu10 from '~/assets/image/logoDu10.png'
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccoutnItem';
 import Button from '~/components/Button';
 import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles)
-
+const currentUser = false
 const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
-        title: 'English'
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English'
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Việt Nam'
+                }
+            ]
+        }
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -36,10 +51,20 @@ function Header() {
             setSearchResult([]);
         }, 0);
     }, []);
+    //handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                //handle change language
+                break;
+            default:
+        }
+    }
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <img src={images.logo} alt="Tiktok" />
+                <img className={cx('logoDu10')} src={logoDu10} alt="logo" />
                 <Tippy
                     interactive
                     visible={searchResult.length > 0}
@@ -66,11 +91,24 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}>
-                    <Button text>Upload</Button>
-                    <Button primary>Log in</Button>
 
-                    <Menu items={MENU_ITEMS}>
+                <div className={cx('actions')}>
+                    {currentUser ? (
+                        <flagment>
+                            <button >
+                                <FontAwesomeIcon icon={faCloudUpload} />
+                            </button>
+                            <button >
+                                <FontAwesomeIcon icon={faMessage} />
+                            </button>
+                        </flagment>
+                    ) : (
+                        <flagment>
+                            <Button text>Upload</Button>
+                            <Button primary>Log in</Button>
+                        </flagment>
+                    )}
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
